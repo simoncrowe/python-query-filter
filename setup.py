2a26d6625ad02801c2b18b7af21ff0a08524cae4
Simon Crowe <simon.r.crowe@protonmail.com>
2020-11-21 12:08:58 +0000

    Added basic files.

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="query-filters",
+    version="0.1.0",
+    author="Simon Crowe",
+    author_email="simon.r.crowe@pm.me",
+    description="Python's filter function with composable queries.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/simoncrowe/python-query-filter",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
